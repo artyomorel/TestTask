09031d7c5700bd3f37ab6b15b93b65a474519b63
Artyom <artyomorel707@gmail.com>
2021-03-19 15:15:27 +0300

    feat(TestTask.Web): Update Validation Birthday

--- TestTask.MVC/ValidationAttributes/RangeDateAttribute.cs
+++ TestTask.MVC/ValidationAttributes/RangeDateAttribute.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace TestTask.MVC.ValidationAttributes
 {
@@ -7,11 +9,15 @@ namespace TestTask.MVC.ValidationAttributes
     {
         public override bool IsValid(object value)
         {
-            if (value == null || (DateTime)value > DateTime.Now)
+            if(!Regex.IsMatch((string)value, "^([0]?[1-9]|[1|2][0-9]|[3][0|1])[.-]([0]?[1-9]|[1][0-2])[.-]([0-9]{4}|[0-9]{2})$"))
             {
                 return false;
             }
-            return true;
+            
+            int[] valueTime = value.ToString().Split(".").Select(x=>Convert.ToInt32(x)).ToArray();
+
+            DateTime time = new DateTime(valueTime[2],valueTime[1], valueTime[0]);
+            return time < DateTime.Now;
         }
     }
 }
