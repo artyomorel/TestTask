eb822466a5e2dae6d6f70248c7ffc7279efcb91e
Artyom <artyomorel707@gmail.com>
2021-03-19 08:51:46 +0300

    feat( TestTask.Web) : Add Mvc

--- TestTask.MVC/Startup.cs
+++ TestTask.MVC/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +13,17 @@ namespace TestTask.MVC
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly IConfiguration _configurtaion;
+
+        public Startup(IConfiguration configurtaion)
+        {
+            _configurtaion = configurtaion;
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +38,7 @@ namespace TestTask.MVC
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
