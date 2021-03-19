14dec816e225c56efeb3abd4c96681386253fac3
Artyom <artyomorel707@gmail.com>
2021-03-19 09:49:43 +0300

    feat (TestTask.Web) : Add View table

---
+++ TestTask.MVC/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTask.MVC.Models
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Snils { get; set; }
+    }
+}
